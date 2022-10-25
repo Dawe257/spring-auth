2607108dad74ba03a3dbb262acb74d2fe87985b3
David Zhenetl <david.zhenetl@gmail.com>
2022-10-25 11:32:36 +0300

    add exception handler

--- src/main/java/com/dzhenetl/springauth/controller/AuthorizationController.java
+++ src/main/java/com/dzhenetl/springauth/controller/AuthorizationController.java
@@ -1,7 +1,13 @@
 package com.dzhenetl.springauth.controller;
 
+import com.dzhenetl.springauth.exception.InvalidCredentials;
+import com.dzhenetl.springauth.exception.UnauthorizedUser;
 import com.dzhenetl.springauth.model.Authorities;
 import com.dzhenetl.springauth.service.AuthorizationService;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +25,16 @@ public class AuthorizationController {
     @GetMapping("/authorize")
     public List<Authorities> getAuthorities(@RequestParam("user") String user, @RequestParam("password") String password) {
         return service.getAuthorities(user, password);
+    }
+
+    @ExceptionHandler(UnauthorizedUser.class)
+    public ResponseEntity<String> unauthorizedHandler(UnauthorizedUser e) {
+        LoggerFactory.getLogger(UnauthorizedUser.class).warn(e.getMessage());
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(InvalidCredentials.class)
+    public ResponseEntity<String> invalidCredentialsHandler(InvalidCredentials e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
